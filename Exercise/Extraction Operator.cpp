0640a1a78d7bd0348bf62abf46bf9d33c6c109da
moinshahmadar <moinsha8840@gmail.com>
2025-07-12 09:16:22 +0530

    Update Extraction Operator.cpp

--- Exercise/Extraction Operator.cpp
+++ Exercise/Extraction Operator.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main(){
-    int x;
+    int j;
     cout<<"Enter a Number: ";
-    cin>> x;
-    cout<<"You Entered: "<<x<<endl;
+    cin>> j;
+    cout<<"You Entered: "<<j<<endl;
     return 0;
 }
