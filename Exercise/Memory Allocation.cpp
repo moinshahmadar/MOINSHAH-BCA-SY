3d8c5ae6a5323e9f9532e9b106d2aa262b485bd0
moinshahmadar <moinsha8840@gmail.com>
2025-07-12 09:20:03 +0530

    Update Memory Allocation.cpp

--- Exercise/Memory Allocation.cpp
+++ Exercise/Memory Allocation.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 
 int main() {
-	int* arr = new int[5]; // Allocate memory for an array of 5 integers
+	int* arr = new int[3]; // Allocate memory for an array of 3 integers
 // Initialize the array
-	for (int i = 0; i<5; ++i) {
+	for (int i = 0; i<3; ++i) {
 		arr[i] = i + 1;
 	}
 // Print the array elements
-	for (int i = 0; i<5; ++i) {
-		std::cout << arr[i] <<" "; // Output: 1 2 3 4 5
+	for (int i = 0; i<3; ++i) {
+		std::cout << arr[i] <<" "; // Output: 1 2 3
 	}
 	std::cout << std::endl;
 	delete[] arr; // Deallocate the array memory
