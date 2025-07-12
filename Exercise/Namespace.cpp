7755d1a339ed7fca7c61be829851f41150407740
moinshahmadar <moinsha8840@gmail.com>
2025-07-12 09:22:32 +0530

    Update Namespace.cpp

--- Exercise/Namespace.cpp
+++ Exercise/Namespace.cpp
@@ -1,17 +1,17 @@
 #include<iostream>
 // Define a namespace called Math
 namespace Math {
-	int add(int a, int b) {
-		return a + b;
+	int add(int j, int k) {
+		return j + k;
 	}
    }
 int main() {
 // Use the add function from the Math namespace
- 	std::cout <<"Sum: "<<Math::add(5, 3) << std::endl;
+ 	std::cout <<"Sum: "<<Math::add(4, 3) << std::endl;
 
 // Alternatively, with 'using' directive
 	using namespace Math;
-	std::cout <<"Sum: "<<add(7, 2) << std::endl;
+	std::cout <<"Sum: "<<add(5, 2) << std::endl;
 
 	return 0;
 }
