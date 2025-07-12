04d61ca86edbf8bce594c3ec77e80584fc0e86ae
moinshahmadar <moinsha8840@gmail.com>
2025-07-12 09:14:58 +0530

    Update Main.cpp

--- Calculator/Main.cpp
+++ Calculator/Main.cpp
@@ -4,37 +4,37 @@ using namespace std;
 
 int main() {
     char op;
-    int num1, num2, result;
+    int number1, number2, result;
 
     cout << "Enter Number 1: ";
-    cin >> num1;
+    cin >> number1;
 
     cout << "Enter Number 2: ";
-    cin >> num2;
+    cin >> number2;
 
     cout << "Enter an operator (+, -, *, /): ";
     cin >> op;
 
     switch(op) {
         case '+':
-            result = num1 + num2;
-            cout << "Result: " << num1 << " + " << num2 << " = " << result << endl;
+            result = number1 + number2;
+            cout << "Result: " << number1 << " + " << num2 << " = " << result << endl;
             break;
 
         case '-':
-            result = num1 - num2;
-            cout << "Result: " << num1 << " - " << num2 << " = " << result << endl;
+            result = number1 - number2;
+            cout << "Result: " << number1 << " - " << num2 << " = " << result << endl;
             break;
 
         case '*':
-            result = num1 * num2;
-            cout << "Result: " << num1 << " * " << num2 << " = " << result << endl;
+            result = number1 * number2;
+            cout << "Result: " << number1 << " * " << num2 << " = " << result << endl;
             break;
 
         case '/':
             if (num2 != 0) {
-                result = num1 / num2;
-                cout << "Result: " << num1 << " / " << num2 << " = " << result << endl;
+                result = number1 / number2;
+                cout << "Result: " << number1 << " / " << number2 << " = " << result << endl;
             } else {
                 cout << "Error: Division by zero is not allowed." << endl;
             }
